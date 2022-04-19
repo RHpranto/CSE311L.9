7f874904a760e4bd9ab62b9e74132dd9e0576851
RH Pranto <68651875+RHpranto@users.noreply.github.com>
2022-04-19 10:37:54 +0600

    Add files via upload

---
+++ pranto part/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer text-center bg-light">
+	© Copyright <span id="demo"></span> <span class="brand">NSU Blood Bank. </span> All Rights Reserved.
+	<script>
+
+    </script>
+</footer>
